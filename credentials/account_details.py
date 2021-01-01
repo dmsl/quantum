c9be403b1843f1206f94347861a3dba3f1f0227e
Sodos <sotirisloizidisano@gmail.com>
2021-01-01 18:29:42 +0200

    removed data

--- credentials/account_details.py
+++ credentials/account_details.py
@@ -2,7 +2,7 @@
 # hub, group and project if you want to access research backends.
 
 # Normal account
-account_token_open = '998094f1e74f1d73b7b00f2a90e808c9d30b3619d4325abda074367393d789d4f7093e1b9a40a825ec1cc3aa48a8f8008cbd87b6cec3f34d3fb752248c149250'
+account_token_open = ''
 
 # Research account
 account_token_research = None
